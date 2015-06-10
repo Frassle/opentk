0a8381fa8f7eb1db971a1dd8af79a46889bc05d3
Fraser Waters <frassle@gmail.com>
2015-06-10 21:08:40 +0100

    Write out "vulkan" for extension attribute

--- Source/Converter/VulkanXmlParser.cs
+++ Source/Converter/VulkanXmlParser.cs
@@ -176,6 +176,7 @@ namespace OpenTK.Convert
 
                 var function = new XElement("function",
                     new XAttribute("name", protoName),
+                    new XAttribute("extension", "vulkan"), // TODO: Should be worked out from feature element
                     new XElement("returns", new XAttribute("type", protoType)));
 
                 foreach (var param in command.Elements("param"))
