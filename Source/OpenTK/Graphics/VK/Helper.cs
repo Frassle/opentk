987064ddbe5eade8869d5f246a1a3da934e7f98d
Fraser Waters <frassle@gmail.com>
2015-07-29 22:08:54 +0100

    Add VK helper class

---
+++ Source/OpenTK/Graphics/VK/Helper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenTK.Graphics.Vulkan
+{
+    /// <summary>
+    /// Provides access to Vulkan 1.x methods for the core profile.
+    /// </summary>
+    public sealed partial class VK
+    {
+        const string Library = "vulkan.dll";
+        static readonly object sync_root = new object();
+
+        static IntPtr[] EntryPoints;
+        static byte[] EntryPointNames;
+        static int[] EntryPointNameOffsets;
+    }
+}
