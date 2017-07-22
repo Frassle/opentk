29d64b49cf08a092b8229018e1b81f9e68439642
Fraser Waters <frassle@gmail.com>
2017-07-22 14:33:32 +0100

    Add DisplayDevice test program

---
+++ tests/Test.Platform.DisplayDevice/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Test.Platform.DisplayDevice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var indices = new OpenTK.DisplayIndex[]
+            {
+                OpenTK.DisplayIndex.Primary,
+
+                OpenTK.DisplayIndex.First,
+                OpenTK.DisplayIndex.Second,
+                OpenTK.DisplayIndex.Third,
+                OpenTK.DisplayIndex.Fourth,
+                OpenTK.DisplayIndex.Fifth,
+                OpenTK.DisplayIndex.Sixth,
+            };
+
+            while (!Console.KeyAvailable)
+            {
+                Console.Clear();
+
+                foreach (var index in indices)
+                {
+                    Console.WriteLine("================================================================================");
+                    Console.WriteLine(index);
+
+                    var display = OpenTK.DisplayDevice.GetDisplay(index);
+                    if (display != null)
+                    {
+                        Console.WriteLine(display);
+                        Console.WriteLine("Primary: {0}", display.IsPrimary);
+                        Console.WriteLine("Current Resolution: {0}", display.CurrentResolution);
+                    }
+                    else
+                    {
+                        Console.WriteLine("GetDisplay returned null");
+                    }
+                }
+
+                System.Threading.Thread.Sleep(TimeSpan.FromSeconds(5));
+            }
+        }
+    }
+}
